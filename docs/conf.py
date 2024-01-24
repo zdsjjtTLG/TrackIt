ca625b00254778a21d4f2606239cb239847632c1
tangkai <tangkai@zhechengdata.com>
2024-01-24 17:12:23 +0800

    modify docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,46 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'gotrackit'
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# The master toctree document.
+master_doc = 'index'
+
+project = 'GoTrackIt'
 copyright = '2024, Kai Tang'
 author = 'Kai Tang'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = '0.1'
+version = '0.1.0'
 
-extensions = []
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    "myst_parser",
+]
+
+source_suffix = {'.rst':'restructuredtext',
+                 '.md': 'markdown'}
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# -- Options for HTML output
 
+html_theme = 'sphinx_rtd_theme'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
