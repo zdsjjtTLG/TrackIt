5b88048d954ff7da1185d1fcfc0beb2c03b9b64b
tangkai <tangkai@zhechengdata.com>
2024-05-06 21:25:46 +0800

    fix bug of judging crs.srs

--- src/gotrackit/gps/GpsXfer.py
+++ src/gotrackit/gps/GpsXfer.py
@@ -35,7 +35,7 @@ class Route2Gps(object):
         :return:
         """
 
-        assert path_gdf.crs.srs == 'EPSG:4326'
+        assert path_gdf.crs.srs.upper() == 'EPSG:4326'
         assert len(path_o_time_df) >= 1
         assert {path_id_field, seq_field, time_cost_field, geometry_field}.issubset(set(path_gdf.columns))
         assert {path_id_field, o_time_field}.issubset(set(path_o_time_df.columns))
