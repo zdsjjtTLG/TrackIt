da96f75ce201c02ff377c623e5887f3a70d67e52
tangkai <tangkai@zhechengdata.com>
2024-06-20 10:45:08 +0800

    Allow matching result table to output user fields

--- src/gotrackit/gps/LocGps.py
+++ src/gotrackit/gps/LocGps.py
@@ -12,13 +12,15 @@ from ..map.Net import Net
 from datetime import timedelta
 from ..tools.geo_process import prj_inf
 from ..tools.geo_process import segmentize
-from ..GlobalVal import GpsField, NetField, PrjConst
 from shapely.geometry import Point, Polygon, LineString
+from ..GlobalVal import GpsField, NetField, PrjConst, MarkovField
 
 
 gps_field = GpsField()
 net_field = NetField()
 prj_const = PrjConst()
+markov_field = MarkovField()
+
 
 lng_field = gps_field.LNG_FIELD
 lat_field = gps_field.LAT_FIELD
@@ -36,6 +38,7 @@ dense_geo_field = gps_field.DENSE_GEO
 n_seg_field = gps_field.N_SEGMENTS
 geo_crs = prj_const.PRJ_CRS
 sub_group_field = gps_field.SUB_GROUP_FIELD
+ori_seq_field = gps_field.ORIGIN_POINT_SEQ_FIELD
 
 
 class GpsPointsGdf(object):
@@ -43,7 +46,7 @@ class GpsPointsGdf(object):
     def __init__(self, gps_points_df: pd.DataFrame = None, buffer: float = 200.0,
                  time_format: str = '%Y-%m-%d %H:%M:%S', time_unit: str = 's',
                  plane_crs: str = 'EPSG:32649', dense_gps: bool = True, dense_interval: float = 25.0,
-                 dwell_l_length: float = 10.0, dwell_n: int = 3):
+                 dwell_l_length: float = 10.0, dwell_n: int = 3, user_filed_list: list[str] = None):
         """
 
         :param gps_points_df: gps数据dataframe, agent_id, lng, lat, time
@@ -54,7 +57,10 @@ class GpsPointsGdf(object):
         :param dwell_l_length: 停留点识别距离阈值
         :param dwell_n: 连续dwell_n个点的距离小于dwell_l_length就被识别为停留点
         :param dense_interval: 加密间隔(相邻GPS点的直线距离小于dense_interval即会进行加密)
+        :param user_filed_list
         """
+        if user_filed_list:
+            assert set(user_filed_list).issubset(gps_points_df.columns), '指定的用户字段不存在!'
         self.geo_crs = geo_crs
         self.buffer = buffer
         self.__crs = self.geo_crs
@@ -65,42 +71,59 @@ class GpsPointsGdf(object):
         self.dwell_n = dwell_n
         self.__gps_point_dis_dict = dict()
         gps_points_df.reset_index(inplace=True, drop=True)
-        self.__gps_points_gdf = gps_points_df
-        self.agent_id = self.__gps_points_gdf.at[0, gps_field.AGENT_ID_FIELD]
-        self.check()
-        self.__gps_points_gdf[gps_field.GEOMETRY_FIELD] = \
-            self.__gps_points_gdf[[gps_field.LNG_FIELD, gps_field.LAT_FIELD]].apply(lambda p: Point(p), axis=1)
-        self.__gps_points_gdf = gpd.GeoDataFrame(self.__gps_points_gdf, geometry=gps_field.GEOMETRY_FIELD,
-                                                 crs=self.geo_crs)
+        self.agent_id = gps_points_df.at[0, gps_field.AGENT_ID_FIELD]
+        self.check(gps_points_df=gps_points_df)
+        gps_points_df[gps_field.GEOMETRY_FIELD] = \
+            gps_points_df[[gps_field.LNG_FIELD, gps_field.LAT_FIELD]].apply(lambda p: Point(p), axis=1)
+        gps_points_gdf = gpd.GeoDataFrame(gps_points_df, geometry=gps_field.GEOMETRY_FIELD, crs=self.geo_crs)
+        del gps_points_df
         try:
-            self.__gps_points_gdf[gps_field.TIME_FIELD] = \
-                pd.to_datetime(self.__gps_points_gdf[gps_field.TIME_FIELD], format=time_format)
+            gps_points_gdf[gps_field.TIME_FIELD] = \
+                pd.to_datetime(gps_points_gdf[gps_field.TIME_FIELD], format=time_format)
         except ValueError:
             print(rf'time column does not match format {time_format}, try using time-unit: {time_unit}')
-            if self.__gps_points_gdf[time_field].dtype == object:
-                self.__gps_points_gdf[time_field] = self.__gps_points_gdf[time_field].astype(float)
-            self.__gps_points_gdf[gps_field.TIME_FIELD] = \
-                pd.to_datetime(self.__gps_points_gdf[gps_field.TIME_FIELD], unit=time_unit)
+            if gps_points_gdf[time_field].dtype == object:
+                gps_points_gdf[time_field] = gps_points_gdf[time_field].astype(float)
+            gps_points_gdf[gps_field.TIME_FIELD] = \
+                pd.to_datetime(gps_points_gdf[gps_field.TIME_FIELD], unit=time_unit)
 
-        self.__gps_points_gdf.sort_values(by=[gps_field.TIME_FIELD], ascending=[True], inplace=True)
-        self.__gps_points_gdf[gps_field.POINT_SEQ_FIELD] = [i for i in range(len(self.__gps_points_gdf))]
-        self.__gps_points_gdf.reset_index(inplace=True, drop=True)
+        gps_points_gdf.sort_values(by=[gps_field.TIME_FIELD], ascending=[True], inplace=True)
+        gps_points_gdf[gps_field.POINT_SEQ_FIELD] = [i for i in range(len(gps_points_gdf))]
+        gps_points_gdf.reset_index(inplace=True, drop=True)
+
+        self.__source_gps_points_gdf = gps_points_gdf.copy()  # 存储最原始的GPS信息
+
+        self.__user_gps_info = pd.DataFrame()
+        user_filed_list = list() if user_filed_list is None else user_filed_list
+        self.user_filed_list = list(
+            set(user_filed_list) - {agent_field, gps_field.POINT_SEQ_FIELD, gps_field.SUB_SEQ_FIELD,
+                                    time_field, gps_field.LOC_TYPE, net_field.LINK_ID_FIELD,
+                                    net_field.FROM_NODE_FIELD, net_field.TO_NODE_FIELD, net_field.DIRECTION_FIELD,
+                                    lng_field, lat_field, geometry_field, 'prj_lng', 'prj_lat', markov_field.PRJ_GEO,
+                                    markov_field.DIS_TO_NEXT})
+
+        self.__user_gps_info = gps_points_gdf[
+            [gps_field.POINT_SEQ_FIELD] + self.user_filed_list].copy()  # user_diy_info
+        self.__user_gps_info[gps_field.LOC_TYPE] = 'source'
+
+        self.__gps_points_gdf = gps_points_gdf[
+            [gps_field.POINT_SEQ_FIELD, agent_field, time_field, geometry_field]].copy()
+
         self.to_plane_prj()
         self.generate_plain_xy()
+
         self.gps_adj_dis_map = dict()
         self.gps_seq_time_map = dict()
         self.gps_seq_geo_map = dict()
         self.gps_rou_buffer = None
 
-        # 存储最原始的GPS信息
-        self.__source_gps_points_gdf = self.__gps_points_gdf.copy()
-
         self.done_diff_heading = False
 
-    def check(self):
+    @staticmethod
+    def check(gps_points_df: pd.DataFrame = None):
         assert {gps_field.LNG_FIELD, gps_field.LAT_FIELD,
                 gps_field.AGENT_ID_FIELD, gps_field.TIME_FIELD}.issubset(
-            set(self.__gps_points_gdf.columns)), \
+            set(gps_points_df.columns)), \
             rf'GPS数据字段有误, 请至少包含如下字段: {gps_field.AGENT_ID_FIELD, gps_field.LNG_FIELD, gps_field.LAT_FIELD, gps_field.TIME_FIELD}'
 
     def generate_plain_xy(self):
@@ -146,10 +169,19 @@ class GpsPointsGdf(object):
         should_be_dense_gdf = gpd.GeoDataFrame(should_be_dense_gdf, geometry=geometry_field, crs=self.crs)
         should_be_dense_gdf[gps_field.PLAIN_X] = should_be_dense_gdf[geometry_field].apply(lambda geo: geo.x)
         should_be_dense_gdf[gps_field.PLAIN_Y] = should_be_dense_gdf[geometry_field].apply(lambda geo: geo.y)
+        self.__gps_points_gdf[ori_seq_field] = self.__gps_points_gdf[gps_field.POINT_SEQ_FIELD]
         self.__gps_points_gdf = pd.concat([self.__gps_points_gdf, should_be_dense_gdf])
         self.__gps_points_gdf.sort_values(by=time_field, ascending=True, inplace=True)
         self.__gps_points_gdf.reset_index(inplace=True, drop=True)
         self.__gps_points_gdf[gps_field.POINT_SEQ_FIELD] = [i for i in range(len(self.__gps_points_gdf))]
+
+        if not self.__user_gps_info.empty:
+            self.__gps_points_gdf[ori_seq_field] = self.__gps_points_gdf[ori_seq_field].fillna(-1).astype(int)
+            ori_now_map = {_ori: _now for _ori, _now in zip(self.__gps_points_gdf[ori_seq_field],
+                                                            self.__gps_points_gdf[gps_field.POINT_SEQ_FIELD])}
+            self.__user_gps_info[gps_field.POINT_SEQ_FIELD] = self.__user_gps_info[gps_field.POINT_SEQ_FIELD].map(
+                ori_now_map).fillna(-1).astype(int)
+        del self.__gps_points_gdf[ori_seq_field]
 
     def calc_adj_dis_gap(self) -> None:
         # 距离差
@@ -185,7 +217,7 @@ class GpsPointsGdf(object):
 
     def rolling_average(self, window: int = 2):
         """
-        滑动窗口降噪
+        滑动窗口降噪, 执行该操作后, 不支持匹配结果表中输出用于自定义字段
         :param window: 窗口大小
         :return:
         """
@@ -214,6 +246,9 @@ class GpsPointsGdf(object):
         self.__gps_points_gdf[gps_field.TIME_FIELD] = \
             pd.to_datetime(self.__gps_points_gdf[gps_field.TIME_FIELD], unit='s')
         self.__gps_points_gdf = gpd.GeoDataFrame(self.__gps_points_gdf, geometry=gps_field.GEOMETRY_FIELD, crs=self.crs)
+        self.__user_gps_info = self.__gps_points_gdf[[gps_field.POINT_SEQ_FIELD]].copy()
+        self.__user_gps_info[gps_field.LOC_TYPE] = 'source'
+        self.user_filed_list = []
 
     def calc_diff_heading(self):
         if self.done_diff_heading:
@@ -238,6 +273,10 @@ class GpsPointsGdf(object):
     @property
     def gps_gdf(self) -> gpd.GeoDataFrame:
         return self.__gps_points_gdf.copy()
+
+    @property
+    def user_info(self) -> pd.DataFrame:
+        return self.__user_gps_info.copy()
 
     @property
     def gps_seq_time(self) -> dict:
