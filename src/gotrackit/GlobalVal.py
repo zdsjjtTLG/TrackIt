0f90425dfd66de8a6ed9d08641a081f74083e430
tangkai <tangkai@zhechengdata.com>
2024-06-21 17:10:51 +0800

    add match_heading、route_dis

--- src/gotrackit/GlobalVal.py
+++ src/gotrackit/GlobalVal.py
@@ -103,8 +103,10 @@ class MarkovField(object):
         self.PRJ_GEO = 'prj_p'
         self.DIS_TO_NEXT = 'dis_to_next'
         self.HEADING_GAP = 'heading_gap'
+        self.MATCH_HEADING = 'match_heading'
         self.USED_HEADING_GAP = 'used_heading_gap'
         self.EMISSION_P = 'emission_p'
+        self.DRIVING_L = 'route_dis'
 
 
 class OdField(object):
