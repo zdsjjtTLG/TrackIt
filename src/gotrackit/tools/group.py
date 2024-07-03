0d6a1f4d2f60a94f3db453995417504b78f060ed
tangkai <tangkai@zhechengdata.com>
2024-07-03 10:13:36 +0800

    opt codes

--- src/gotrackit/tools/group.py
+++ src/gotrackit/tools/group.py
@@ -10,11 +10,15 @@ def cut_group(obj_list: list[object] = None, n: int = None) -> list[list[object]
     df = pd.DataFrame({'obj': obj_list})
     df['id'] = [i for i in range(1, len(df) + 1)]
     df['label'] = list(pd.cut(df['id'], bins=n, labels=[i for i in range(1, n + 1)]))
-    return [df[df['label'] == i]['obj'].to_list() for i in range(1, n + 1)]
+    obj_list = [df[df['label'] == i]['obj'].to_list() for i in range(1, n + 1)]
+    obj_list = [obj for obj in obj_list if obj]
+    return obj_list
 
 
 def cut_group_for_df(df:pd.DataFrame = None, n: int = None) -> list[pd.DataFrame]:
     df['id'] = [i for i in range(1, len(df) + 1)]
     df['label'] = list(pd.cut(df['id'], bins=n, labels=[i for i in range(1, n + 1)]))
     del df['id']
-    return [df[df['label'] == i] for i in range(1, n + 1)]
+    df_list = [df[df['label'] == i] for i in range(1, n + 1)]
+    df_list = [df for df in df_list if not df.empty]
+    return df_list
