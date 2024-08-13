31cf8e79f7b3c6d2ea2d1b36b4370c33860881b3
tangkai <tangkai@zhechengdata.com>
2024-08-13 18:15:23 +0800

    add encoding para

--- src/gotrackit/tools/save_file.py
+++ src/gotrackit/tools/save_file.py
@@ -10,7 +10,7 @@ import geopandas as gpd
 
 
 def save_file(data_item: pd.DataFrame or gpd.GeoDataFrame = None, out_fldr: str = None, file_name: str = None,
-              file_type: str = None) -> None:
+              file_type: str = None, encoding: str = 'gbk') -> None:
     assert file_type in ['csv', 'shp', 'geojson']
     if out_fldr is None:
         pass
@@ -19,11 +19,11 @@ def save_file(data_item: pd.DataFrame or gpd.GeoDataFrame = None, out_fldr: str 
             data_item.to_csv(os.path.join(out_fldr, file_name + '.csv'), encoding='utf_8_sig', index=False)
         elif file_type == 'geojson':
             if isinstance(data_item, gpd.GeoDataFrame):
-                data_item.to_file(os.path.join(out_fldr, file_name + '.geojson'), encoding='gbk')
+                data_item.to_file(os.path.join(out_fldr, file_name + '.geojson'), encoding=encoding)
             else:
                 raise ValueError('gpd.GeoDataFrame才能存储为geojson')
         else:
             if isinstance(data_item, gpd.GeoDataFrame):
-                data_item.to_file(os.path.join(out_fldr, file_name + '.shp'), encoding='gbk')
+                data_item.to_file(os.path.join(out_fldr, file_name + '.shp'), encoding=encoding)
             else:
                 raise ValueError('gpd.GeoDataFrame才能存储为shp')
