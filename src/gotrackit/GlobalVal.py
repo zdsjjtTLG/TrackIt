530acf121d0c83bd1f871abcffd3170a54c1370e
tangkai <tangkai@zhechengdata.com>
2024-09-15 23:57:52 +0800

    trip visualization function

--- src/gotrackit/GlobalVal.py
+++ src/gotrackit/GlobalVal.py
@@ -521,6 +521,198 @@ class KeplerConfig(object):
             }
         }
 
+        self.__BASE_LAYER_CONFIG = {
+            'id': 'kb29zg',
+            'type': 'geojson',
+            'config': {
+                'dataId': 'mix',
+                'label': 'mix',
+                'color': [
+                    18,
+                    147,
+                    154
+                ],
+                'highlightColor': [
+                    252,
+                    242,
+                    26,
+                    255
+                ],
+                'columns': {
+                    'geojson': 'geometry'
+                },
+                'isVisible': True,
+                'visConfig': {
+                    'opacity': 0.8,
+                    'strokeOpacity': 0.8,
+                    'thickness': 0.1,
+                    'strokeColor': [
+                        221,
+                        178,
+                        124
+                    ],
+                    'colorRange': {
+                        'name': 'Custom Palette',
+                        'type': 'custom',
+                        'category': 'Custom',
+                        'colors': [
+                            '#FFC300',
+                            '#438ecd'
+                        ],
+                        'reversed': True
+                    },
+                    'strokeColorRange': {
+                        'name': 'Global Warming',
+                        'type': 'sequential',
+                        'category': 'Uber',
+                        'colors': [
+                            '#5A1846',
+                            '#900C3F',
+                            '#C70039',
+                            '#E3611C',
+                            '#F1920E',
+                            '#FFC300'
+                        ]
+                    },
+                    'radius': 3,
+                    'sizeRange': [
+                        0,
+                        10
+                    ],
+                    'radiusRange': [
+                        0,
+                        50
+                    ],
+                    'heightRange': [
+                        0,
+                        500
+                    ],
+                    'elevationScale': 5,
+                    'enableElevationZoomFactor': True,
+                    'stroked': False,
+                    'filled': True,
+                    'enable3d': False,
+                    'wireframe': False
+                },
+                'hidden': False,
+                'textLabel': [
+                    {
+                        'field': None,
+                        'color': [
+                            255,
+                            255,
+                            255
+                        ],
+                        'size': 18,
+                        'offset': [
+                            0,
+                            0
+                        ],
+                        'anchor': 'start',
+                        'alignment': 'center'
+                    }
+                ]
+            },
+            'visualChannels': {
+                'colorField': {
+                    'name': 'type',
+                    'type': 'string'
+                },
+                'colorScale': 'ordinal',
+                'strokeColorField': None,
+                'strokeColorScale': 'quantile',
+                'sizeField': None,
+                'sizeScale': 'linear',
+                'heightField': None,
+                'heightScale': 'linear',
+                'radiusField': None,
+                'radiusScale': 'linear'
+            }
+        }
+
+        self.__GLB_MAP_CONFIG = {
+            'version': 'v1',
+            'config': {
+                'visState': {
+                    'filters': [],
+                    'layers': [],
+                    'interactionConfig': {
+                        'tooltip': {
+                            'fieldsToShow': {
+                            },
+                            'compareMode': False,
+                            'compareType': 'absolute',
+                            'enabled': True
+                        },
+                        'brush': {
+                            'size': 2.7,
+                            'enabled': False
+                        },
+                        'geocoder': {
+                            'enabled': False
+                        },
+                        'coordinate': {
+                            'enabled': True
+                        }
+                    },
+                    'layerBlending': 'normal',
+                    'splitMaps': [],
+                    'animationConfig': {
+                        'currentTime': None,
+                        'speed': 0.5
+                    }
+                },
+                'mapState': {
+                    'bearing': 24,
+                    'dragRotate': True,
+                    'latitude': 34.23188690328708,
+                    'longitude': 108.94371457924842,
+                    'pitch': 50,
+                    'zoom': 15,
+                    'isSplit': False
+                },
+                'mapStyle': {
+                    'styleType': 'dark',
+                    'topLayerGroups': {},
+                    'visibleLayerGroups': {
+                        'label': True,
+                        'road': False,
+                        'border': False,
+                        'building': True,
+                        'water': True,
+                        'land': True,
+                        '3d building': False
+                    },
+                    'threeDBuildingColor': [
+                        9.665468314072013,
+                        17.18305478057247,
+                        31.1442867897876
+                    ],
+                    'mapStyles': {}
+                }
+            }
+        }
+
+        self.__TIME_FILTER_CONFIG = {
+            'id': 'fd18q2cbg',
+            'dataId': [
+                'mix'
+            ],
+            'name': [
+                'time'
+            ],
+            'type': 'timeRange',
+            'value': [
+                1652372040.0,
+                1652373265.0
+            ],
+            'enlarged': False,
+            'plotType': 'histogram',
+            'animationWindow': 'incremental',
+            'yAxis': None,
+            'speed': 0.2
+        }
+
         self.BASE_LINK_NAME = 'base_link'
         self.BASE_NODE_NAME = 'base_node'
         self.ERROR_XFER = 'error_xfer'
@@ -532,11 +724,19 @@ class KeplerConfig(object):
     def get_base_config(self):
         return copy.deepcopy(self.__BASE_CONFIG)
 
+    def get_glb_map_config(self):
+        return copy.deepcopy(self.__GLB_MAP_CONFIG)
+
     def get_polygon_config(self):
         return copy.deepcopy(self.__POLYGON_CONFIG)
 
     def get_point_config(self):
         return copy.deepcopy(self.__POINT_CONFIG)
+
+    def get_base_layer_config(self):
+        return copy.deepcopy(self.__BASE_LAYER_CONFIG)
+    def get_time_filter_config(self):
+        return copy.deepcopy(self.__TIME_FILTER_CONFIG)
 
 
 class PrjConst(object):
