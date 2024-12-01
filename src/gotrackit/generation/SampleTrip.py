925aba13f2082372309cae35e9d25a46860807c2
tangkai <tangkai@zhechengdata.com>
2024-12-01 11:23:41 +0800

    opt code

--- src/gotrackit/generation/SampleTrip.py
+++ src/gotrackit/generation/SampleTrip.py
@@ -15,16 +15,17 @@ class TripGeneration(object):
     def __init__(self, net: Net = None, time_step: float = 0.1, speed_miu: float = 12.0,
                  speed_sigma: float = 3.6, save_gap: int = 1, loc_frequency: float = 2.0,
                  loc_error_sigma: float = 40.0, loc_error_miu: float = 0.0):
-        """
+        """TripGeneration类初始化
 
-        :param net: 路网对象
-        :param time_step: 仿真步长(s)
-        :param speed_miu: 仿真速度均值(m/s)
-        :param speed_sigma: 仿真速度标准差(m/s)
-        :param save_gap: 每多少仿真步保存一次真实轨迹数据
-        :param loc_frequency: 每多少s进行一次GPS定位
-        :param loc_error_sigma: 定位误差均值(m)
-        :param loc_error_miu: 定位误差标准差(m)
+        Args:
+            net: 路网对象
+            time_step: 仿真步长(s)
+            speed_miu: 仿真速度均值(m/s)
+            speed_sigma: 仿真速度标准差(m/s)
+            save_gap: 每多少仿真步保存一次真实轨迹数据，整数
+            loc_frequency: 每多少s进行一次GPS定位，该值必须大于仿真步长
+            loc_error_sigma: 定位误差标准差(m)
+            loc_error_miu: 定位误差均值(m)
         """
         self.net = net
         self.time_step = time_step
@@ -40,6 +41,26 @@ class TripGeneration(object):
                             start_year: int = 2022, start_month: int = 5, start_day: int = 15, start_hour: int = 10,
                             start_minute: int = 20, start_second: int = 12,
                             file_type: str = 'geojson'):
+        """TripGeneration的类方法：generate_rand_trips函数
+        - 生成随机路径的trip
+
+        Args:
+            trip_num:
+            instant_output: 是否即时输出，即是否每计算完一次行程就存储GPS数据文件、逐帧轨迹文件
+            out_fldr: 存储输出文件的目录
+            time_format: 输出GPS数据的时间列的格式
+            agent_flag: 标志字符
+            start_year: 车辆出发的起始年
+            start_month: 车辆出发的起始月
+            start_day: 车辆出发的起始日
+            start_hour: 车辆出发的起始小时
+            start_minute: 车辆出发的起始分钟
+            start_second: 车辆出发的起始秒
+            file_type: 输出文件的类型，shp或者geojson
+
+        Returns:
+            真实轨迹数据表、GPS轨迹数据表、混合数据表
+        """
 
         return self.__generate_trips(trip_num=trip_num, instant_output=instant_output, out_fldr=out_fldr,
                                      time_format=time_format, agent_flag=agent_flag, start_year=start_year,
@@ -51,6 +72,26 @@ class TripGeneration(object):
                           start_year: int = 2022, start_month: int = 5, start_day: int = 15, start_hour: int = 10,
                           start_minute: int = 20, start_second: int = 12,
                           file_type: str = 'geojson'):
+        """TripGeneration的类方法：generate_od_trips函数
+        - 生成指定OD路径的trip
+
+        Args:
+            od_set: 起始节点OD对，按照起始节点进行路径搜索后生成trip，形如[(o_node, d_node), …] 例如[(12, 23), (34, 111)]，表示生成2个trip，分别为节点12到节点23的最短路径、节点34到节点111的最短路径
+            instant_output: 是否即时输出，即是否每计算完一次行程就存储GPS数据文件、逐帧轨迹文件
+            out_fldr: 存储输出文件的目录
+            time_format: 输出GPS数据的时间列的格式
+            agent_flag: 标志字符
+            start_year: 车辆出发的起始年
+            start_month: 车辆出发的起始月
+            start_day: 车辆出发的起始日
+            start_hour: 车辆出发的起始小时
+            start_minute: 车辆出发的起始分钟
+            start_second: 车辆出发的起始秒
+            file_type: 输出文件的类型，shp或者geojson
+
+        Returns:
+            真实轨迹数据表、GPS轨迹数据表、混合数据表
+        """
         if od_set is None or not od_set:
             return None
         return self.__generate_trips(od_set=od_set, instant_output=instant_output, out_fldr=out_fldr,
@@ -64,6 +105,26 @@ class TripGeneration(object):
                                 start_year: int = 2022, start_month: int = 5, start_day: int = 15, start_hour: int = 10,
                                 start_minute: int = 20, start_second: int = 12,
                                 file_type: str = 'geojson'):
+        """TripGeneration的类方法：generate_destined_trips函数
+        - 生成指定路径的trip
+
+        Args:
+            node_paths: 路径节点序列，形如[[node1, node2, node3, …], [node5, node7, node9, …],…] 例如[(12, 90, 9012, 123), (3412, 23, 112, 23), (34, 344, 111)]，表示生成3个trip，每个trip的节点序列已经指定好
+            instant_output: 是否即时输出，即是否每计算完一次行程就存储GPS数据文件、逐帧轨迹文件
+            out_fldr: 存储输出文件的目录
+            time_format: 输出GPS数据的时间列的格式
+            agent_flag: 标志字符
+            start_year: 车辆出发的起始年
+            start_month: 车辆出发的起始月
+            start_day: 车辆出发的起始日
+            start_hour: 车辆出发的起始小时
+            start_minute: 车辆出发的起始分钟
+            start_second: 车辆出发的起始秒
+            file_type: 输出文件的类型，shp或者geojson
+
+        Returns:
+            真实轨迹数据表、GPS轨迹数据表、混合数据表
+        """
         if node_paths is None or not node_paths:
             return None
         return self.__generate_trips(node_paths=node_paths, instant_output=instant_output, out_fldr=out_fldr,
