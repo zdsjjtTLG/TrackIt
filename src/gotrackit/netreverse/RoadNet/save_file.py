8c3bbead09b9326271151d57a6412081f56cfd5b
tangkai <tangkai@zhechengdata.com>
2024-12-26 16:33:13 +0800

    opt code

--- src/gotrackit/netreverse/RoadNet/save_file.py
+++ src/gotrackit/netreverse/RoadNet/save_file.py
@@ -17,15 +17,17 @@ def save_file(data_item: pd.DataFrame or gpd.GeoDataFrame = None, out_fldr: str 
     if out_fldr is None:
         pass
     else:
+        if not os.path.exists(out_fldr):
+            os.makedirs(out_fldr)
         if file_type == 'csv':
             data_item.to_csv(os.path.join(out_fldr, file_name + '.csv'), encoding='utf_8_sig', index=False)
         elif file_type == 'geojson':
             if isinstance(data_item, gpd.GeoDataFrame):
                 data_item.to_file(os.path.join(out_fldr, file_name + '.geojson'), encoding='gbk')
             else:
-                raise ValueError('gpd.GeoDataFrame才能存储为geojson')
+                raise ValueError('only gpd.GeoDataFrame can be stored as geojson file')
         else:
             if isinstance(data_item, gpd.GeoDataFrame):
                 data_item.to_file(os.path.join(out_fldr, file_name + '.shp'), encoding='gbk')
             else:
-                raise ValueError('gpd.GeoDataFrame才能存储为shp')
+                raise ValueError('only gpd.GeoDataFrame can be stored as shape file')
